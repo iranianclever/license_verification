64567d24bf0c7f1224a99958386550268901bc39
iranianclever <iranianclever@gmail.com>
2023-11-10 18:58:33 +0330

    working on login

--- app/main.py
+++ app/main.py
@@ -77,3 +77,89 @@ class User(UserMixin):
 
 
 user = User(0)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+@limiter.limit('20 per minute')
+def login():
+    """ User login: only for admin user (System has no other user than admin)
+    Note: there is a 10 tries per minute limitation to admin login to avoid minimize password factoring """
+    if current_user.is_authenticated:
+        return redirect('/')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == config.PASSWORD and username == config.USERNAME:
+            login_user(user)
+            return redirect('/')
+        else:
+            return abort(401)
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    """ Logs out the admin user """
+    logout_user()
+    flash('Logged out', 'success')
+    return redirect('/login')
+
+
+# handle login failed
+@app.errorhandler(401)
+def unauthorized(error):
+    """ Handling login failures """
+    flash('Login problem', 'danger')
+    return redirect('/login')
+
+
+# callback to reload the user object
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """ Redirect to 404 page in page not found status. """
+    return render_template('404.html'), 404
+
+
+@app.route('/v1/ok')
+def health_check():
+    """ Will return message: OK when called. for monitoring systems. """
+    ret = {'message': 'ok'}
+    return jsonify(ret), 200
+
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def home():
+    """ Creates database if method is post otherwise shows the homepage with some stats see import_database_from_excel() for more details on database creation """
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part', 'danger')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file', 'danger')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filename.replace(' ', '_')
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            subprocess.Popen(['python', 'import_db.py', file_path])
+            flash(
+                'File uploaded. Will be imported soon. Follow from DB Status page.', 'info')
+            return redirect('/')
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
